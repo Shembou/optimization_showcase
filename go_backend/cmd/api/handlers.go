cf92dd8d2f4ecc4e07e024a514932af854a8adfb
Przemyslaw Kaminski <sshembou@gmail.com>
2025-08-19 17:15:23 +0200

    WIP: add websocket handler definition

--- go_backend/cmd/api/handlers.go
+++ go_backend/cmd/api/handlers.go
@@ -3,7 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func (app *application) websocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		app.logger.Println("Failed to initialize websocket connection")
+		return
+	}
+	conn.
+
+}
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
--- go_backend/go.mod
+++ go_backend/go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
 	golang.org/x/net v0.42.0 // indirect
